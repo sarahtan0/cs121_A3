f01eb0052e048de01f26d9c10b6dbd42219f5e27
Stanley Sha <ssha2@uci.edu>
2025-02-27 19:02:54 +0000

    added query tokenization

--- index_search.py
+++ index_search.py
@@ -1,12 +1,24 @@
 import json
 import mmap
 import time
+import re
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
 
 with open('final_offset.json', 'r') as o_file:
     offset_index = json.load(o_file)
 
 with open('final_index.txt', 'r') as i_file:
     mem_map = mmap.mmap(i_file.fileno(), 0, access=mmap.ACCESS_READ)
+
+def tokenize_query(query):
+    ps = PorterStemmer()
+    
+    tokens = re.findall(r'\b[a-zA-Z0-9]+\b', query.lower())
+    stemmed_tokens = [ps.stem(token) for token in tokens]
+    
+    return stemmed_tokens
 
 def retrieve(word):
     if word not in offset_index:
@@ -19,7 +31,8 @@ def retrieve(word):
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
-    result = retrieve("ace")
+    query_list = tokenize_query("masters of software engineering")
+    for token in query_list:
+        result = retrieve(token)
     elapsed = (time.perf_counter() - start_time) * 1000  # in ms
-    print(result)
     print(f"Elapsed time: {elapsed:.2f} ms")
